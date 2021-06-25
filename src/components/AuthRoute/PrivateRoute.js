ac02ac710a27fe6ec289f6792ed55aaa43b34cff
Ujjwal <ujjwal2022.cs19@nsut.ac.in>
2021-06-25 15:56:02 +0530

    netlify error test

--- src/components/AuthRoute/PrivateRoute.js
+++ src/components/AuthRoute/PrivateRoute.js
@@ -8,7 +8,7 @@ export default function PrivateRoute({ component: Component, ...rest }) {
     <Route
       {...rest}
       render={(props) => {
-        return currentUser ? <Component {...props} /> : <Redirect to="/auth" />;
+        return currentUser ? <Component {...props} /> : <Redirect to="/" />;
       }}
     ></Route>
   );
