e91d8bd6e5e2bedb92a838043c75cd3f176f37d4
Ujjwal <ujjwal2022.cs19@nsut.ac.in>
2021-06-25 16:51:55 +0530

    error notifications

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,14 +1,31 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useHistory } from 'react-router';
 import { useAuth } from '../../firebase/AuthContext';
+import avatar from './man.png';
 import './navbar.scss';
 import Cart from '../Cart/cart';
+import { store } from 'react-notifications-component';
 
 function Navbar() {
   const { currentUser } = useAuth();
   const { logout } = useAuth();
   const history = useHistory();
   console.log(currentUser);
+
+  useEffect(() => {
+    store.addNotification({
+      title: 'Our servers are running slow. You might experience a delay in getting notifications.',
+      message: 'Please be patient.',
+      type: 'info',
+      insert: 'top',
+      container: 'top-full',
+      animationIn: ['animate__animated', 'animate__fadeIn'],
+      animationOut: ['animate__animated', 'animate__fadeOut'],
+      dismiss: {
+        duration: 4000
+      }
+    });
+  }, []);
 
   async function SignOutClick() {
     try {
@@ -52,7 +69,7 @@ function Navbar() {
       <div className="header-title">Bookify</div>
       <div className="profile">
         <div className="user-profile">
-          <img src={currentUser.photoURL} alt="" className="user-img" />
+          <img src={currentUser.photoURL || avatar} alt="" className="user-img" />
         </div>
         <Cart trigger={trigger} />
         <div className="profile-menu">
